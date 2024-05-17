f0a0c9cef9f5fd55252636041ae5151570f57fed
BogushF <boga1602@gmail.com>
2024-05-17 14:50:06 +0200

    html+css

---
+++ src/js/1-timer.js
@@ -0,0 +1,11 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
+
+const buttonStart = document.querySelector('[data-start]');
+const inputDate = document.querySelector('#datetime-picker');
+const timerDays = document.querySelector('[data-days]');
+const timerHours = document.querySelector('[data-hours]');
+const timerMinutes = document.querySelector('[data-minutes]');
+const timerSeconds = document.querySelector('[data-seconds]');
