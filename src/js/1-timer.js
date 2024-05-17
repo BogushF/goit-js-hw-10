24f9c43556eee8336e38b6e698098797c2d3f60b
BogushF <boga1602@gmail.com>
2024-05-17 15:56:48 +0200

    update timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -9,3 +9,69 @@ const timerDays = document.querySelector('[data-days]');
 const timerHours = document.querySelector('[data-hours]');
 const timerMinutes = document.querySelector('[data-minutes]');
 const timerSeconds = document.querySelector('[data-seconds]');
+
+buttonStart.disabled = true;
+let userSelectDate = null;
+let intervalId = null;
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    if (selectedDates[0].getTime() > Date.now()) {
+      buttonStart.disabled = false;
+      userSelectDate = selectedDates[0].getTime();
+    } else {
+      iziToast.error({
+        title: 'Error',
+        position: 'topRight',
+        message: 'Please choose a date in the future',
+      });
+      buttonStart.disabled = true;
+    }
+  },
+};
+
+flatpickr('#datetime-picker', options);
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
+
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+}
+
+buttonStart.addEventListener('click', () => {
+  buttonStart.disabled = true;
+  inputDate.disabled = true;
+
+  intervalId = setInterval(() => {
+    const diff = userSelectDate - Date.now();
+    const { days, hours, minutes, seconds } = convertMs(diff);
+
+    timerDays.textContent = addLeadingZero(days);
+    timerHours.textContent = addLeadingZero(hours);
+    timerMinutes.textContent = addLeadingZero(minutes);
+    timerSeconds.textContent = addLeadingZero(seconds);
+
+    if (diff < 1000) clearInterval(intervalId);
+  }, 1000);
+});
